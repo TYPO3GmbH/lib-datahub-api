d25ede3ec528b62a5ebbb0ca4c3a26c7a0076129
Benjamin Kott <benjamin.kott@outlook.com>
2020-09-17 11:37:09 +0200

    [TASK] Drop teaser text product (#181)

--- src/Enum/PSLType.php
+++ src/Enum/PSLType.php
@@ -17,7 +17,6 @@ final class PSLType extends AbstractEnum
 {
     public const LOGO = 'logo';
     public const BACKLINK = 'backlink';
-    public const TEASER_TEXT = 'teaser_text';
     public const PROFILE_PAGE = 'profile_page';
     public const MAP_VIEW = 'map_view';
     public const CONTACT_FORM = 'contact_form';
@@ -34,7 +33,6 @@ final class PSLType extends AbstractEnum
     protected static array $optionNames = [
         self::LOGO => 'Logo',
         self::BACKLINK => 'Backlink',
-        self::TEASER_TEXT => 'Teaser Text',
         self::PROFILE_PAGE => 'Profile Page Text',
         self::MAP_VIEW => 'Map View',
         self::CONTACT_FORM => 'Contact Form',
